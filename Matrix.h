8693d98e74d55796741a40cfe317f9d74324a364
0x03A6 <wwz_2007@163.com>
2025-09-28 14:50:17 +0800

    Add matrix add

--- Matrix.h
+++ Matrix.h
@@ -9,8 +9,19 @@ template<typename T, int N, int M>
 class Matrix {
     T vals[N][M];
 public:
-
+    Matrix<T, N, M> operator+(const Matrix<T, N, M>& other) const;
 };
+
+template<typename T, int N, int M>
+Matrix<T, N, M> Matrix<T, N, M>::operator+(const Matrix<T, N, M> &other) const {
+    Matrix<T, N, M> result;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            result.vals[i][j] = vals[i][j] + other.vals[i][j];
+        }
+    }
+    return result;
+}
 
 
 #endif //MY2048_MATRIX_H
